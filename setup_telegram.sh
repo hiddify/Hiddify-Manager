8039b4eff4c08441460df625e7dbba9ef8cced97
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-09-30 14:25:38 +0200

    Update setup_telegram.sh

--- setup_telegram.sh
+++ setup_telegram.sh
@@ -3,7 +3,9 @@ useradd --no-create-home -s /usr/sbin/nologin tgproxy
 cd /opt/
 git clone https://github.com/alexbers/mtprotoproxy 
 cd  mtprotoproxy
-sed -i 's/00000000000000000000000000000001/550c6d91546b3e79e312ee2537b37ee8/g' config.py
+if [[ "$1" ]]; then
+sed -i "s/00000000000000000000000000000001/$1/g" config.py
+fi
 echo 'TLS_DOMAIN = "www.google.com"'>> config.py
 wget https://raw.githubusercontent.com/hiddify/config/main/mtproxy.service
 mv mtproxy.service /etc/systemd/system/
