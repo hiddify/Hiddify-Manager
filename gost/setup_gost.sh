b056ef136f2ce472b381c79f43328fc48f340da8
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-09-30 23:40:15 +0200

    Update setup_gost.sh

--- gost/setup_gost.sh
+++ gost/setup_gost.sh
@@ -1,15 +1,24 @@
-apk install apt-transport-https ca-certificates curl wget gnupg-agent software-properties-common git nginx certbot python3-certbot-nginx
+#!/bin/bash
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl wget gnupg-agent software-properties-common git nginx certbot python3-certbot-nginx
+
 mkdir -p /opt/gost
 cd /opt/gost
+
 wget https://github.com/ginuerzh/gost/releases/download/v2.11.4/gost-linux-amd64-2.11.4.gz
 gunzip gost*
 mv gost* gost
 chmod +x gost
+
 wget https://raw.githubusercontent.com/hiddify/config/main/gost/gost.service
 wget https://raw.githubusercontent.com/hiddify/config/main/gost/nginx.conf
 
 rm /etc/nginx/site-available/default
 ln -s $(pwd)/nginx.conf /etc/nginx/site-available/default 
+ln -s $(pwd)/gost.service /etc/systemd/system/gost.service
+
+
 
 if [[ "$1" ]]; then
 	sed -i "s/nginxusersecret/$1/g" nginx.conf
@@ -22,7 +31,9 @@ if [[ "$2" ]]; then
 	sed -i "s/nginxserverhost/$2/g" nginx.conf
 	certbot --nginx --register-unsafely-without-email -d $domain --non-interactive --agree-tos  --https-port 444
 fi
+
 openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 3650 -nodes -subj "/C=GB/ST=London/L=London/O=Google Trust Services LLC/CN=www.google.com"
-ln -s $(pwd)/gost.service /etc/systemd/system/gost.service
-sudo systemctl enable gost.service
-sudo systemctl start gost.service
+
+systemctl enable gost.service
+systemctl start gost.service
+systemctl restart nginx
