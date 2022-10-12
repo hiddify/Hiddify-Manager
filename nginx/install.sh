64191bdeda035366826fc6c932c24e4647548c52
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-10-12 10:33:37 +0200

    Create install.sh

---
+++ nginx/install.sh
@@ -0,0 +1,31 @@
+USER_SECRET=$1
+DOMAIN=$2
+IP=$(curl -Lso- https://api.ipify.org);
+echo $IP
+
+apt-get install -y apt-transport-https ca-certificates git curl wget gnupg-agent software-properties-common git nginx certbot python3-certbot-nginx
+
+mkdir -p /opt/nginx
+cd /opt/nginx
+
+rm /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
+
+ln -s $(pwd)/web.conf /etc/nginx/conf.d/web.conf
+mkdir -p /etc/nginx/stream.d/ && ln -s $(pwd)/sni-proxy.conf /etc/nginx/stream.d/sni-proxy.conf
+mkdir -p /etc/nginx/stream.d/ && ln -s $(pwd)/signal.conf /etc/nginx/stream.d/signal.conf
+
+if ! grep -Fxq "include /etc/nginx/stream.d/*.conf;" /etc/nginx/nginx.conf; then
+  echo "include /etc/nginx/stream.d/*.conf;">>/etc/nginx/nginx.conf;
+fi
+
+sed -i "s/defaultusersecret/$USER_SECRET/g" web.conf
+sed -i "s/defaultserverip/$IP/g" web.conf
+
+sed -i "s/defaultserverhost/$DOMAIN/g" web.conf
+sed -i "s/defaultserverhost/$DOMAIN/g" sni-proxy.conf
+certbot --nginx --register-unsafely-without-email -d $DOMAIN --non-interactive --agree-tos  --https-port 444
+sed -i "s/listen 444 ssl;/listen 444 ssl http2;/" web.conf
+echo "https://$DOMAIN/$USER_SECRET/">use-link
+
+systemctl restart nginx
