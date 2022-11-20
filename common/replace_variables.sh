1754598ca1b0f7211c5215d7d3dd27051a05296e
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-11-20 20:58:25 +0000

    fix bug

--- common/replace_variables.sh
+++ common/replace_variables.sh
@@ -7,21 +7,21 @@ IP=$(curl -Lso- https://api.ipify.org);
 
 for template_file in $(find . -name "*.template"); do
     out_file=${template_file/.template/}
-    
+    cp $template_file $out_file
 
-    sed "s|defaultusersecret|$USER_SECRET|g" $template_file >$out_file
-    sed "s|defaultuserguidsecret|$GUID_SECRET|g" $template_file >$out_file
-    sed "s|defaultcloudprovider|$CLOUD_PROVIDER|g" $template_file >$out_file
-    sed "s|defaultserverip|$IP|g" $template_file >$out_file
-    sed "s|defaultserverhost|$ROOT_DOMAIN|g" $template_file >$out_file
-    sed "s|telegramadtag|$TELEGRAM_AD_TAG|g" $template_file >$out_file
-    sed "s|telegramtlsdomain|$TELEGRAM_FAKE_TLS_DOMAIN|g" $template_file >$out_file
-    sed "s|sstlsdomain|$SS_FAKE_TLS_DOMAIN|g" $template_file >$out_file
-    sed "s|hextelegramdomain|$HEX_TELEGRAM_DOMAIN|g" $template_file >$out_file
+    sed -i "s|defaultusersecret|$USER_SECRET|g" $template_file 
+    sed -i "s|defaultuserguidsecret|$GUID_SECRET|g" $template_file 
+    sed -i "s|defaultcloudprovider|$CLOUD_PROVIDER|g" $template_file 
+    sed -i "s|defaultserverip|$IP|g" $template_file 
+    sed -i "s|defaultserverhost|$ROOT_DOMAIN|g" $template_file 
+    sed -i "s|telegramadtag|$TELEGRAM_AD_TAG|g" $template_file 
+    sed -i "s|telegramtlsdomain|$TELEGRAM_FAKE_TLS_DOMAIN|g" $template_file 
+    sed -i "s|sstlsdomain|$SS_FAKE_TLS_DOMAIN|g" $template_file 
+    sed -i "s|hextelegramdomain|$HEX_TELEGRAM_DOMAIN|g" $template_file 
 
-    sed "s|GITHUB_REPOSITORY|$GITHUB_REPOSITORY|g" $template_file >$out_file
-    sed "s|GITHUB_USER|$GITHUB_USER|g" $template_file >$out_file
-    sed "s|GITHUB_BRANCH|$GITHUB_BRANCH_OR_TAG|g" $template_file >$out_file
+    sed "s|GITHUB_REPOSITORY|$GITHUB_REPOSITORY|g" $template_file 
+    sed "s|GITHUB_USER|$GITHUB_USER|g" $template_file 
+    sed "s|GITHUB_BRANCH|$GITHUB_BRANCH_OR_TAG|g" $template_file 
     
     
 
