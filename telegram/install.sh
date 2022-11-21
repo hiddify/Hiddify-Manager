779181efce945deff0869f7d92aa186087454ef5
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-11-21 00:11:55 +0000

    fix error

--- telegram/install.sh
+++ telegram/install.sh
@@ -5,8 +5,8 @@ useradd --no-create-home -s /usr/sbin/nologin tgproxy
 
 git clone https://github.com/alexbers/mtprotoproxy 
 cd  mtprotoproxy
-sed "s/00000000000000000000000000000001/defaultusersecret/g" config.py config.py.template
-echo 'AD_TAG="telegramadtag">>config.py.template
+sed "s/00000000000000000000000000000001/defaultusersecret/g" config.py > config.py.template
+echo 'AD_TAG="telegramadtag"'>>config.py.template
 echo 'TLS_DOMAIN = "telegramtlsdomain"'>> config.py.template
 sed -i 's/PORT = 443/PORT = 449/g' config.py.template
 
