434cc38f05e40d20b479f6b907ad44c6e46a73c1
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-11-30 11:53:01 +0000

    fix aggregating users

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -1,5 +1,5 @@
 apt install -y python3-pip cron
-pip install git+https://github.com/hiddify/hiddify-monitoring
+pip install -U git+https://github.com/hiddify/hiddify-monitoring
 pip install bottle pandas numpy
 
 systemctl stop hiddify_monitoring_web.service
