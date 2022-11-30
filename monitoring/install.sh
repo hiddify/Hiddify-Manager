3c96288ae312510079ac98a76e9bad4514455a0e
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-11-30 01:10:36 +0000

    fix cron bug

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -6,7 +6,8 @@ systemctl stop hiddify_monitoring_web.service
 
 ln -s $(pwd)/hiddify_monitoring_web.service /etc/systemd/system/hiddify_monitoring_web.service
 
-echo "0,15,30,45 * * * * bash $(pwd)/cron.sh">/etc/cron.d/hiddify-monitoring
+echo "0,15,30,45 * * * * root bash $(pwd)/cron.sh">/etc/cron.d/hiddify-monitoring
+service cron reload
 
 systemctl enable hiddify_monitoring_web.service
 systemctl restart hiddify_monitoring_web.service
