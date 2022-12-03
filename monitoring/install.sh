a47788e5083c434e8f18743e723510bd12a8a03f
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-03 11:10:58 +0000

    fix typo

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -3,7 +3,7 @@ pip install -U git+https://github.com/hiddify/hiddify-monitoring
 pip install bottle pandas numpy
 
 systemctl stop hiddify_monitoring_web.service
-
+mkdir -p ../log/stats/
 ln -s $(pwd)/hiddify_monitoring_web.service /etc/systemd/system/hiddify_monitoring_web.service
 
 echo "0,15,30,45 * * * * root $(pwd)/cron.sh" > /etc/cron.d/hiddify-monitoring
