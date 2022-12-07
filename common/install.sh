a2a783dc177c8cf3ecae151d2b45920116deaba1
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-07 13:07:29 +0000

    add enable firewall

--- common/install.sh
+++ common/install.sh
@@ -4,3 +4,12 @@ ln -s $(pwd)/sysctl.conf /etc/sysctl.d/ss-opt.conf
 sysctl --system
 
 bash google-bbr.sh
+
+
+
+ if [[ $ENABLE_FIREWALL == true ]]; then
+  iptables -I INPUT -p tcp --dport 443 -j ACCEPT
+  iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+  iptables -I INPUT -p tcp --dport 22 -j ACCEPT
+  iptables -P INPUT DROP
+fi
