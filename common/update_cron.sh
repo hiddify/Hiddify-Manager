b55e9b69230739ee13d1b78f044955cba5f7e139
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-09 21:07:17 +0000

    update cron

--- common/update_cron.sh
+++ common/update_cron.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-cd /opt/GITHUB_REPOSITORY/
+cd $( dirname -- "$0"; )/../
 
 changed=0
 git pull --dry-run 2>&1 | grep -q -v 'Already up-to-date.' && changed=1
