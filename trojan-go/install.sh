747b9def1a128296ad5ee129363d8b9c3deccf7c
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-11 11:59:23 +0000

    reload all daemon

--- trojan-go/install.sh
+++ trojan-go/install.sh
@@ -9,3 +9,5 @@ wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-
 unzip trojan-go-linux-* trojan-go
 
 ln -s $(pwd)/trojan-go.service /etc/systemd/system/trojan-go.service
+
+systemctl daemon-reload
