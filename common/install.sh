7dd27f1df1058c19d491e3e6fe768cf974b67570
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-11 12:29:05 +0000

    disable ipv6 to avoid issues

--- common/install.sh
+++ common/install.sh
@@ -6,6 +6,10 @@ ln -s $(pwd)/sysctl.conf /etc/sysctl.d/ss-opt.conf
 sysctl --system
 
 
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
 
 bash google-bbr.sh
 
