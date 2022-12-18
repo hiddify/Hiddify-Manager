78b8a2d8e623735da15f89bebe7d1290a15531ef
kontorol <60671553+kontorol@users.noreply.github.com>
2022-12-17 18:01:02 -0800

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -11,7 +11,9 @@ if [[ $ONLY_IPV4 == true ]];then
   sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 fi
 
-bash google-bbr.sh
+if [[ $ENABLE_BBR == true ]]; then
+  bash google-bbr.sh
+fi
 
 function add2iptables(){
   iptables -C $1 || echo "adding rule $1" && iptables -I $1
@@ -23,7 +25,7 @@ function add2iptables(){
   add2iptables "INPUT -p tcp --dport 443 -j ACCEPT"
   add2iptables "INPUT -p udp --dport 53 -j ACCEPT"
   add2iptables "INPUT -p tcp --dport 80 -j ACCEPT"
-  add2iptables "INPUT -p tcp --dport 22 -j ACCEPT"
+  add2iptables "INPUT -p tcp --dport $SSH_PORT -j ACCEPT"
   iptables -P INPUT DROP
   iptables-save > /etc/iptables/rules.v4 
 
