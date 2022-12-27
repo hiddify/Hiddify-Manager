7de7624a1f4ef3d88bb91cd717dff4fad1155204
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-27 08:20:04 +0000

    update telegram proxy for higher performance

--- telegram/run.sh
+++ telegram/run.sh
@@ -1,2 +1,5 @@
-systemctl enable mtproxy.service
-systemctl restart mtproxy.service
+# systemctl enable mtproxy.service
+# systemctl restart mtproxy.service
+
+cd mtproto_proxy
+sudo make update-sysconfig && sudo systemctl reload mtproto-proxy
