5001c4d18ae1bd86f2c8c24d6664fb56462f620b
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-27 15:47:16 +0000

    update telegram proxy

--- telegram/install.sh
+++ telegram/install.sh
@@ -1,26 +1,19 @@
 echo "telegram proxy install.sh $*"
 systemctl stop mtproxy.service
 systemctl disable mtproxy.service
-
-# apt install -y python3 python3-uvloop python3-cryptography python3-socks libcap2-bin
-# useradd --no-create-home -s /usr/sbin/nologin tgproxy
-
-# git clone https://github.com/alexbers/mtprotoproxy 
-# cd  mtprotoproxy
-# sed "s/00000000000000000000000000000001/defaultusersecret/g" config.py > config.py.template
-# echo 'AD_TAG="telegramadtag"'>>config.py.template
-# echo 'TLS_DOMAIN = "telegramtlsdomain"'>> config.py.template
-# sed -i 's/PORT = 443/PORT = 1001/g' config.py.template
-
-# ln -s  $(pwd)/../mtproxy.service /etc/systemd/system/
-
-curl -L -o install_mtproxy.sh https://gist.githubusercontent.com/hiddify/9134259f2df9ff91bb365c629512b6b3/raw/ec42ccfd96431686425a8f4e4e16a07d12062e82/install_mtproxy.sh 
-bash install_mtproxy.sh -p 1001 -s 00000000000000000000000000000001  -t 00000000000000000000000000000002 -a tls -d google.com
+systemctl disable mtproto-proxy.service
 
 
-cp prod-sys.config-base mtproto_proxy/config/prod-sys.config.template
+apt install -y git curl build-essential libssl-dev zlib1g-dev
+ln -s  $(pwd)/mtproxy.service /etc/systemd/system/
 
+git clone https://github.com/TelegramMessenger/MTProxy
+cd MTProxy
 
-# IP=$(curl -Lso- https://api.ipify.org);
+make 
 
-# echo "https://t.me/proxy?server=$IP&port=443&secret=ee$16d61696c2e676f6f676c652e636f6d">use-link
+cd objs/bin
+
+curl -s https://core.telegram.org/getProxySecret -o proxy-secret
+curl -s https://core.telegram.org/getProxyConfig -o proxy-multi.conf
+
--- telegram/run.sh
+++ telegram/run.sh
@@ -1,6 +1,3 @@
-# systemctl enable mtproxy.service
-# systemctl restart mtproxy.service
+systemctl enable mtproxy.service
+systemctl restart mtproxy.service
 
-cd mtproto_proxy
-sudo make update-sysconfig 
-sudo systemctl restart mtproto-proxy
