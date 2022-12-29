301fb387d5dc882f3f3877df0b4cda0b0ecb7c9d
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-29 01:24:46 +0000

    update wget

--- shadowsocks/install.sh
+++ shadowsocks/install.sh
@@ -4,7 +4,7 @@ systemctl stop ss-faketls.service
 
 apt-get install -y  shadowsocks-libev simple-obfs
 
-wget https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-linux-$(dpkg --print-architecture)-v1.3.2.tar.gz
+wget -c https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-linux-$(dpkg --print-architecture)-v1.3.2.tar.gz
 tar xvzf v2ray-plugin-linux-*
 mv v2ray-plugin_linux_$(dpkg --print-architecture) v2ray-plugin_linux
 
--- trojan-go/install.sh
+++ trojan-go/install.sh
@@ -4,7 +4,7 @@ systemctl stop trojan-go.service
 pkg=$(dpkg --print-architecture)
 [[ $pkg == 'arm64' ]] && pkg='arm'
 
-wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-$pkg.zip
+wget -c https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-$pkg.zip
 
 unzip -o trojan-go-linux-* trojan-go
 
