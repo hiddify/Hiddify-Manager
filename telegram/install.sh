acc2844c4cfdee4b32e7afa8cb6a6fffaae16182
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-29 00:56:16 +0000

    update go version

--- telegram/install.sh
+++ telegram/install.sh
@@ -5,7 +5,12 @@ systemctl stop mtproto-proxy.service
 systemctl disable mtproto-proxy.service
 
 
-apt install -y git curl make golang golang-go
+apt install -y git curl make 
+
+wget -c https://go.dev/dl/go1.19.linux-amd64.tar.gz  
+tar -xvf go1.19.linux-amd64.tar.gz   
+export PATH=$(pwd)/go/bin:$PATH
+
 ln -s  $(pwd)/mtproxy.service /etc/systemd/system/
 
 git clone https://github.com/9seconds/mtg/
