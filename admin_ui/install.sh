11e5510c6db1d4eda80f386657991b61c9f815d1
Hiddify <114227601+hiddify@users.noreply.github.com>
2023-01-01 17:20:13 +0000

    fix install

--- admin_ui/install.sh
+++ admin_ui/install.sh
@@ -1,11 +1,8 @@
-systemctl stop hiddify-admin.service
-systemctl disable hiddify-admin.service
 
 apt install -y python3-pip
 pip install bottle
 
 
 
-pkill -9 python3
-ln -s $(pwd)/hiddify-admin.service /etc/systemd/system/hiddify-admin.service
+
 
--- admin_ui/run.sh
+++ admin_ui/run.sh
@@ -1,3 +1,4 @@
-systemctl daemon-reload
+
+ln -s $(pwd)/hiddify-admin.service /etc/systemd/system/hiddify-admin.service
 systemctl enable hiddify-admin.service
 systemctl restart hiddify-admin.service
