7e0d64a10e0f1f3147e5783b5c7943443aefedfa
hiddify <hiddify@gmail.com>
2023-01-04 23:46:58 +0330

    improve performance by removing nginx sni proxy

--- xray/run.sh
+++ xray/run.sh
@@ -1,5 +1,6 @@
 mv /usr/local/etc/xray/config.json /usr/local/etc/xray/config.json.old
-ln -s $(pwd)/xtls-config.json /usr/local/etc/xray/config.json
+# ln -s $(pwd)/xtls-config.json /usr/local/etc/xray/config.json
+ln -s $(pwd)/xtls-sni-config.json /usr/local/etc/xray/config.json
 sed -i "s/^User=/#User=/g" /etc/systemd/system/xray.service
 
 systemctl daemon-reload
