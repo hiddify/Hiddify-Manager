ec97df11b0bf5a114516a7d870661024c004b0df
hiddify <hiddify@gmail.com>
2023-01-08 14:09:36 +0330

    add more info on status

--- status.sh
+++ status.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 function main(){
+	systemctl status --no-pager nginx xray |cat
+
 for s in **/*.service v2ray netdata nginx xray;do
 	s=${s##*/}
 	s=${s%%.*}
 	printf "%-30s %-30s \n" $s $(systemctl is-active $s)
 done
+
+
 
 }
 mkdir -p log/system/
