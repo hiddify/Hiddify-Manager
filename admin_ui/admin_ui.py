a3744b7a8b2d53461111181a27b32cdab1066300
hiddify <hiddify@gmail.com>
2023-01-15 00:00:17 +0330

    fix compatibility issue

--- admin_ui/admin_ui.py
+++ admin_ui/admin_ui.py
@@ -43,13 +43,16 @@ def reinstall(complete_install=True):
     # os.system(f'cd {config_dir};{env} ./install.sh &')
     # rc = subprocess.call(f"cd {config_dir};./{file} & disown",shell=True)
     subprocess.Popen(f"{config_dir}/{file}",cwd=f"{config_dir}",start_new_session=True)
+    
     return template("result",data={
                         "out-type":"success",
                         "out-msg":f"Success! Please wait around {6 if complete_install else 2} minutes to make sure everything is updated. Then, please save your proxy links which are <br>"+
-                                f"<h1>User Link</h1><a href='https://{configs['MAIN_DOMAIN']}/{configs['USER_SECRET']}/'>https://{configs['MAIN_DOMAIN']}/{configs['USER_SECRET']}/</a><br>"+
+                                # f"<h1>User Link</h1><a href='https://{configs['MAIN_DOMAIN']}/{configs['USER_SECRET']}/'>https://{configs['MAIN_DOMAIN']}/{configs['USER_SECRET']}/</a><br>"+
                                 f"<h1>Secure Admin Link</h1><a href='https://{configs['MAIN_DOMAIN']}/{configs['ADMIN_SECRET']}/'>https://{configs['MAIN_DOMAIN']}/{configs['ADMIN_SECRET']}/</a><br>"+
+                                f"<h6>Alternative Admin Link</h6><a href='https://{server_ip}.sslip.io/{configs['ADMIN_SECRET']}/'>http://{server_ip}/{configs['ADMIN_SECRET']}/</a><br>",
                                 f"<h6>Alternative Admin Link</h6><a href='http://{server_ip}/{configs['ADMIN_SECRET']}/'>http://{server_ip}/{configs['ADMIN_SECRET']}/</a><br>",
                         "log-path":f"https://{configs['MAIN_DOMAIN']}/{configs['ADMIN_SECRET']}/reverselog/0-install.log"
+                        
     })
 
 
--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 cd $( dirname -- "$0"; )
 
 function main(){
-
+    git branch
     changed=0
     git pull --dry-run 2>&1 | grep -q -v 'Already up-to-date.' && changed=1
 
