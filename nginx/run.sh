6944dd4fd5d5362b56f307dd03fa139aa1eaae33
hiddify <hiddify@gmail.com>
2023-01-18 16:36:59 +0330

    fix limit time

--- nginx/run.sh
+++ nginx/run.sh
@@ -14,7 +14,12 @@ else
 fi
 
 if [[ "$FIRST_SETUP" == "true" ]];then
+	
 	TEMP_LINK_VALID_TIME="$(date '+%Y-%m-%dT%H')"
+	nextH="$(printf '%02d' $(($(date '+%H') +1)))"
+	if [[ "$nextH" != "" ]];then
+		TEMP_LINK_VALID_TIME=$(date "+%Y-%m-%dT(%H|$nextH)")
+	fi
 	sed -i "s|TEMP_LINK_VALID_TIME|$TEMP_LINK_VALID_TIME|g" def-link.conf
 fi
 if [[ "$ENABLE_MONITORING" == "false" ]];then
