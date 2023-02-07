e1d750ab95e7fb7b2aa6ce20167baaa3b0090f8b
hiddify <hiddify@gmail.com>
2023-02-07 20:20:28 +0100

    update

--- hiddify-panel/temporary_access.sh
+++ hiddify-panel/temporary_access.sh
@@ -7,11 +7,11 @@ function main(){
     PORT="${1:-9001}"
     echo "we are openning a port on $PORT"
     iptables -I INPUT -p tcp --dport $PORT -j ACCEPT
-
+    kill $(lsof -t -i:$PORT)
     echo 'kill $(lsof -t -i:'$PORT')'| at now + 4 hour
     echo "iptables -D INPUT -p tcp --dport $PORT -j ACCEPT" | at now + 4 hour
 
-    gunicorn -b 0.0.0.0:$PORT 'hiddifypanel:create_app()' &
+    gunicorn --daemon -b 0.0.0.0:$PORT 'hiddifypanel:create_app()' 
 
 
 
