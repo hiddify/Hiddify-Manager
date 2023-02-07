16fd8577dbe0c7ba8524250dad77a6b0357ecec3
hiddify <hiddify@gmail.com>
2023-02-07 20:13:35 +0100

    fix temp access

--- hiddify-panel/temporary_access.sh
+++ hiddify-panel/temporary_access.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 cd $( dirname -- "$0"; )
-PORT= $1 || 9001
-
-iptables -I INPUT -p tcp --dport $PORT -j ACCEPT
-
-echo "kill $(lsof -t -i:$PORT)"| at now + 4 hour
-echo "iptables -D INPUT -p tcp --dport $PORT -j ACCEPT" | at now + 4 hour
-
-gunicorn -b 0.0.0.0:$PORT 'hiddifypanel:create_app()' &
 
 
+
+function main(){
+    PORT="${1:-9001}"
+    echo "we are openning a port on $PORT"
+    iptables -I INPUT -p tcp --dport $PORT -j ACCEPT
+
+    echo 'kill $(lsof -t -i:'$PORT')'| at now + 4 hour
+    echo "iptables -D INPUT -p tcp --dport $PORT -j ACCEPT" | at now + 4 hour
+
+    gunicorn -b 0.0.0.0:$PORT 'hiddifypanel:create_app()' &
+
+
+
+}
+
+
+main $@ |& tee ../log/system/temporary_access.log
