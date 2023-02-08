f47cb915747d68c514ef853a96f6b2f19f2072a2
hiddify <hiddify@gmail.com>
2023-02-08 22:38:45 +0100

    update

--- acme.sh/install.sh
+++ acme.sh/install.sh
@@ -1,7 +1,10 @@
 sudo apt-get install socat
 sudo apt-get uninstall certbot
-mkdir -p /opt/hiddify-config/acme.sh/lib/
-curl -L https://get.acme.sh| sh -s -- home /opt/hiddify-config/acme.sh/lib/ 
-    
+if [ ! -d lib ];then
+    mkdir -p /opt/hiddify-config/acme.sh/lib/
+    curl -L https://get.acme.sh| sh -s -- home /opt/hiddify-config/acme.sh/lib/
+fi
+
+./lib/acme.sh --register-account -m my@example.com
 
 mkdir -p ../ssl/
