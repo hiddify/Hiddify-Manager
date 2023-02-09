73137aaaf4e71efb34be4ba7bd455178c751487b
hiddify <hiddify@gmail.com>
2023-02-09 02:06:49 +0100

    fix tgo

--- other/telegram/tgo/install.sh
+++ other/telegram/tgo/install.sh
@@ -2,17 +2,19 @@ echo "telegram proxy install.sh $*"
 systemctl kill mtproxy.service
 systemctl disable mtproxy.service
 
-sudo add-apt-repository -y ppa:longsleep/golang-backports
-sudo apt update
-apt install -y make golang 
+# sudo add-apt-repository -y ppa:longsleep/golang-backports
+# sudo apt update
+# apt install -y make golang 
 
-# wget -c https://go.dev/dl/go1.19.linux-amd64.tar.gz  
-# tar -xvf go1.19.linux-amd64.tar.gz   
-# export PATH=$(pwd)/go/bin:$PATH
+pkg=$(dpkg --print-architecture)
+
+wget -c https://go.dev/dl/go1.19.linux-$pkg.tar.gz  
+tar -xvf go1.19.linux-amd64.tar.gz   
+export PATH=$(pwd)/go/bin:$PATH
 
 
 
-export GOPATH=/opt/hiddify-config/telegram/go/
+export GOPATH=/opt/hiddify-config/other/telegram/tgo/go/
 git clone https://github.com/9seconds/mtg/
 
 cd mtg
