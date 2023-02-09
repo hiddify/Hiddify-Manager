eca8edb59daf14656c12d18000ca397ae322fed5
hiddify <hiddify@gmail.com>
2023-02-09 01:38:05 +0100

    no daemon gunicorn

--- hiddify-panel/temporary_access.sh
+++ hiddify-panel/temporary_access.sh
@@ -11,7 +11,7 @@ function main(){
     echo 'kill $(lsof -t -i:'$PORT')'| at now + 4 hour
     echo "iptables -D INPUT -p tcp --dport $PORT -j ACCEPT" | at now + 4 hour
 
-    gunicorn --daemon -b 0.0.0.0:$PORT 'hiddifypanel:create_app()' 
+    gunicorn -b 0.0.0.0:$PORT 'hiddifypanel:create_app()' 
 
 
 
