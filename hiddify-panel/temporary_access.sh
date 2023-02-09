313f5a80a59dbb3848700b3855bfaa30a0e22d8c
hiddify <hiddify@gmail.com>
2023-02-09 01:55:09 +0100

    fix background

--- hiddify-panel/temporary_access.sh
+++ hiddify-panel/temporary_access.sh
@@ -11,7 +11,7 @@ function main(){
     echo 'kill $(lsof -t -i:'$PORT')'| at now + 4 hour
     echo "iptables -D INPUT -p tcp --dport $PORT -j ACCEPT" | at now + 4 hour
 
-    gunicorn --preload -b 0.0.0.0:$PORT 'hiddifypanel:create_app()' 
+    echo "gunicorn -D --preload -b 0.0.0.0:$PORT 'hiddifypanel:create_app()' " |at now 
 
 
 
