439aa9ad904656c232cce73b41c14918777c42e9
hiddify <hiddify@gmail.com>
2023-02-09 02:11:22 +0100

    add go cache

--- other/telegram/tgo/install.sh
+++ other/telegram/tgo/install.sh
@@ -8,13 +8,14 @@ systemctl disable mtproxy.service
 
 pkg=$(dpkg --print-architecture)
 
-wget -c https://go.dev/dl/go1.19.linux-$pkg.tar.gz  
-tar -xvf go1.19.linux-amd64.tar.gz   
+wget -q --show-progress -c https://go.dev/dl/go1.19.linux-$pkg.tar.gz  
+tar -xf go1.19.linux-amd64.tar.gz   
 export PATH=$(pwd)/go/bin:$PATH
 
 
 
 export GOPATH=/opt/hiddify-config/other/telegram/tgo/go/
+export GOCACHE=/opt/hiddify-config/other/telegram/tgo/gocache/
 git clone https://github.com/9seconds/mtg/
 
 cd mtg
