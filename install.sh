e45002a4bc7cbcedabfbf90975d0aafc075b8ed1
hiddify <hiddify@gmail.com>
2023-02-15 09:33:04 +0100

    add https for relay domain

--- install.sh
+++ install.sh
@@ -86,7 +86,7 @@ function set_config_from_hpanel(){
         for i in $(seq 0 ``domains.length()``); do
                 domain=$(get domains $i domain)
                 mode=$(get domains $i mode)
-                if [ "$mode"  == "direct" ] || [ "$mode"  == "cdn" ];then
+                if [ "$mode"  == "direct" ] || [ "$mode"  == "cdn" ] || [ "$mode"  == "relay" ];then
                         MAIN_DOMAIN="$domain;$MAIN_DOMAIN"
                 fi
                 if [ "$mode"  = "ss_faketls" ];then
