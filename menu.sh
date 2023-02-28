cb1abf61e416adce7a85c03acc6d1177f96ecc82
hiddify <hiddify@gmail.com>
2023-02-28 22:14:13 +0100

    new: add menu

--- menu.sh
+++ menu.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 cd $( dirname -- "$0"; )
-HEIGHT=15
+HEIGHT=20
 WIDTH=60
 CHOICE_HEIGHT=4
-BACKTITLE="Welcome to Hiddify Panel"
-TITLE="Hiddify Panel"
+BACKTITLE="Welcome to Hiddify Central Panel (config version=$(cat VERSION))"
+TITLE="Hiddify Central Panel"
 MENU="Choose one of the following options:"
 
 OPTIONS=(status "View status of system"
-         apply_configs "Apply the changed configs"
-         install "Reinstall"
          restart "Restart Services without changing the configs"
-         upgrade "Upgrade"
+         log "view system logs"
+         apply_configs "Apply the changed configs"
+         update "Update "
+         admin "Show admin link"
+         install "Reinstall"
+         disable "Disable showing this window on startup"
+         enable "enable showing this window on startup"
          uninstall "Uninstall"
          )
 
@@ -24,5 +28,51 @@ CHOICE=$(dialog --clear \
                 2>&1 >/dev/tty)
 
 clear
+echo "Hiddify: Command $CHOICE"
+echo "=========================================="
+case $CHOICE in 
+    "") exit;;
+    'log')
+        W=() # define working array
+        while read -r line; do # process file by file
+            size=$(ls -lah log/system/$line | awk -F " " {'print $5'})
+            W+=($line "$size")
+        done < <( ls -1 log/system )
+        LOG=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${W[@]}" \
+                2>&1 >/dev/tty)
+        clear
+        echo -e "\033[0m"
+        less +G "log/system/$LOG"
+    ;;
+    "enable")
+        echo "/opt/hiddify-central-config/menu.sh">>~/.bashrc
+        echo "cd /opt/hiddify-central-config/">>~/.bashrc
+        ;;
+    "disable")
+        sed -i "s|/opt/hiddify-central-config/menu.sh||g" ~/.bashrc
+        sed -i "s|cd /opt/hiddify-central-config/||g" ~/.bashrc
+        ;;
+    "admin")
+        (cd hiddify-panel; python3 -m hiddifypanel admin-links)   
+        ;;
+    "status")
+        bash status.sh |less +G
+        ./menu.sh
+        exit
+        ;;
+    *)
+        bash $CHOICE.sh
+esac
 
-bash $CHOICE.sh
+read -p "Press any key to return to menu, press 'q' to exit" -n 1 key
+if [[ $key == 'q' ]];then
+    echo ""
+    exit; 
+fi
+
+./menu.sh
