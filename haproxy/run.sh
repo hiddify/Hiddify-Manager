342d26a396870276f2bffc223cf4a0288186139e
hiddify <hiddify@gmail.com>
2023-03-05 09:53:43 +0100

    fix: fix bug when shadowtls is not enabled

--- haproxy/run.sh
+++ haproxy/run.sh
@@ -1,4 +1,7 @@
 ln -sf $(pwd)/haproxy.cfg /etc/haproxy/haproxy.cfg
-
+if [[ "$SHADOWTLS_FAKEDOMAIN" == "" ]];then
+sed -i "s|server shadowtls_decoy_http |server shadowtls_decoy_http no.com|g" haproxy.cfg
+sed -i "s|server shadowtls_decoy |server shadowtls_decoy no.com|g" haproxy.cfg
+fi
  systemctl reload haproxy
  systemctl start haproxy
