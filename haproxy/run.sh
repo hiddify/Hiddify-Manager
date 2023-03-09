648ca0b6ccc7f0cd2b9bca7e94d44276582e987d
hiddify <hiddify@gmail.com>
2023-03-09 09:25:07 +0100

    fix: port in haproxy

--- haproxy/run.sh
+++ haproxy/run.sh
@@ -4,17 +4,17 @@ sed -i "s|server shadowtls_decoy_http |server shadowtls_decoy_http no.com|g" hap
 sed -i "s|server shadowtls_decoy |server shadowtls_decoy no.com|g" haproxy.cfg
 fi
 
-PORT_80=
+PORT_80=''
 for PORT in ${HTTP_PORTS//,/ };	do 
   PORT_80="$PORT_80\n  bind *:$PORT"
 done
-sed -i "s|bind *:80|$PORT_80|g" haproxy.cfg
+sed -i 's|bind \*:80|'$PORT_80'|g' haproxy.cfg
 
-PORT_443=
+PORT_443=''
 for PORT in ${TLS_PORTS//,/ };	do 
-  PORT_443="$PORT_443\n  bind *:$PORT"
+  PORT_443='$PORT_443\n  bind *:$PORT'
 done
-
+sed -i 's|bind \*:443|'$PORT_443'|g' haproxy.cfg
 
  systemctl reload haproxy
  systemctl start haproxy
