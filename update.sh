854152075288ac03151552e58658ea43635ee675
hiddify <hiddify@gmail.com>
2023-03-11 00:58:52 +0100

    new: force reinstall latest update on update release and update develop

--- update.sh
+++ update.sh
@@ -10,14 +10,16 @@ function get_commit_version(){
 }
 
 function main(){
-    rm -rf sniproxy/*
-    rm -rf caddy/*
+    rm -rf sniproxy
+    rm -rf caddy
     ./hiddify-panel/backup.sh
     UPDATE=0
     if [[ "$1" == "" ]];then
         PACKAGE_MODE=$(cd hiddify-panel;python3 -m hiddifypanel all-configs|jq -r ".hconfigs.package_mode")
+        FORCE=false
     else
         PACKAGE_MODE=$1
+        FORCE=true
     fi
     
     if [[ "$PACKAGE_MODE" == "develop" ]];then
@@ -26,7 +28,7 @@ function main(){
         INSTALL_DIR=$(pip show hiddifypanel |grep Location |awk -F": " '{ print $2 }')
         CURRENT=$(cat $INSTALL_DIR/hiddifypanel/VERSION)
         echo "DEVLEOP: hiddify panel version current=$CURRENT latest=$LATEST"
-        if [[ "$LATEST" != "$CURRENT" ]];then
+        if [[ FORCE == "true" || "$LATEST" != "$CURRENT" ]];then
             pip3 uninstall -y hiddifypanel
             pip3 install -U git+https://github.com/hiddify/HiddifyPanel
             echo $LATEST>$INSTALL_DIR/hiddifypanel/VERSION
@@ -37,7 +39,7 @@ function main(){
         CURRENT=`pip3 freeze |grep hiddifypanel|awk -F"==" '{ print $2 }'`
         LATEST=`lastversion hiddifypanel --at pip`
         echo "hiddify panel version current=$CURRENT latest=$LATEST"
-        if [[ "$CURRENT" != "$LATEST" ]];then
+        if [[ FORCE == "true" || "$CURRENT" != "$LATEST" ]];then
             echo "panel is outdated! updating...."
             pip3 install -U hiddifypanel
             UPDATE=1
@@ -50,7 +52,7 @@ function main(){
     if [[ "$PACKAGE_MODE" == "develop" ]];then
         LAST_CONFIG_VERSION=$(get_commit_version hiddify-config)
         echo "DEVELOP: Current Config Version=$CURRENT_CONFIG_VERSION -- Latest=$LAST_CONFIG_VERSION"
-        if [[ "$CURRENT_CONFIG_VERSION" != "$LAST_CONFIG_VERSION" ]];then
+        if [[ FORCE == "true" || "$CURRENT_CONFIG_VERSION" != "$LAST_CONFIG_VERSION" ]];then
             wget -c https://github.com/hiddify/hiddify-config/archive/refs/heads/main.tar.gz
             rm  -rf nginx/ xray/
             tar xvzf main.tar.gz --strip-components=1
@@ -63,7 +65,7 @@ function main(){
     else 
         LAST_CONFIG_VERSION=$(lastversion hiddify/hiddify-config)
         echo "Current Config Version=$CURRENT_CONFIG_VERSION -- Latest=$LAST_CONFIG_VERSION"
-        if [[ "$CURRENT_CONFIG_VERSION" != "$LAST_CONFIG_VERSION" ]];then
+        if [[ FORCE == "true" || "$CURRENT_CONFIG_VERSION" != "$LAST_CONFIG_VERSION" ]];then
             echo "Config is outdated! updating..."
             wget -c $(lastversion hiddify/hiddify-config --source)
             rm  -rf nginx/ xray/
