2f5284c3d75609a748ca822aaf7893b1b33b8c83
hiddify <hiddify@gmail.com>
2023-04-18 20:29:19 +0200

    fix: reality

--- update.sh
+++ update.sh
@@ -57,7 +57,7 @@ function main(){
         echo "DEVELOP: Current Config Version=$CURRENT_CONFIG_VERSION -- Latest=$LAST_CONFIG_VERSION"
         if [[ FORCE == "true" || "$CURRENT_CONFIG_VERSION" != "$LAST_CONFIG_VERSION" ]];then
             wget -c https://github.com/hiddify/hiddify-config/archive/refs/heads/main.tar.gz
-            rm  -rf nginx/ xray/
+            # rm  -rf nginx/ xray/
             tar xvzf main.tar.gz --strip-components=1
             rm main.tar.gz
             echo $LAST_CONFIG_VERSION > VERSION
@@ -71,7 +71,7 @@ function main(){
         if [[ FORCE == "true" || "$CURRENT_CONFIG_VERSION" != "$LAST_CONFIG_VERSION" ]];then
             echo "Config is outdated! updating..."
             wget  $(lastversion --at github --assets --filter hiddify-config.zip  hiddify/hiddify-config) -O hiddify-config.zip
-            rm  -rf nginx/ xray/
+            # rm  -rf nginx/ xray/
             apt install -y  unzip
             unzip -o hiddify-config.zip
             rm hiddify-config.zip
@@ -84,6 +84,10 @@ function main(){
     fi
     if [[ "$PANEL_UPDATE" == 1 ]];then
         systemctl restart hiddify-panel
+    fi
+
+    if [[ "$PANEL_UPDATE" == 1 &&  $UPDATE == 0 ]];then
+        bash apply_configs.sh
     fi
 }
 
