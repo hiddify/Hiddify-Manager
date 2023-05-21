58716be82ba5147360e98778dc63f8ca4f62309c
hiddify <hiddify@gmail.com>
2023-05-21 10:57:54 +0200

    new: update haproxy to version 2.4

--- haproxy/install.sh
+++ haproxy/install.sh
@@ -8,7 +8,9 @@ systemctl disable haproxy
 pkill -9 haproxy
 pkill -9 sniproxy
 
-apt install haproxy -y
+add-apt-repository ppa:vbernat/haproxy-2.4 -y
+apt update
+apt install haproxy=2.4.\* -y
 
 ln -sf $(pwd)/hiddify-haproxy.service /etc/systemd/system/hiddify-haproxy.service
 systemctl enable hiddify-haproxy.service
