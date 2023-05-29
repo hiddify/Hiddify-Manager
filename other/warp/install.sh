f9c73134d25e7f6212576158c54cc1c6bd38cd21
hiddify <hiddify@gmail.com>
2023-05-29 12:31:36 +0200

    fix: warp

--- other/warp/install.sh
+++ other/warp/install.sh
@@ -1,9 +1,10 @@
 
 if ! [ -f "wgcf-profile.conf" ];then
+    mv wgcf-account.toml wgcf-account.toml.backup
     TAR="https://api.github.com/repos/ViRb3/wgcf/releases/latest"
     ARCH=$(dpkg --print-architecture)
-    URL=$(curl -fsSL ${TAR} | grep 'browser_download_url' | cut -d'"' -f4 | grep linux | grep "${ARCH}")
-    curl -fsSL "${URL}" -o ./wgcf && chmod +x ./wgcf && mv ./wgcf /usr/bin
+    URL=$(curl --connect-timeout 10 -fsSL ${TAR} | grep 'browser_download_url' | cut -d'"' -f4 | grep linux | grep "${ARCH}")
+    curl --connect-timeout 10 -fsSL "${URL}" -o ./wgcf && chmod +x ./wgcf && mv ./wgcf /usr/bin
     wgcf register --accept-tos && wgcf generate   
 fi
 
@@ -13,9 +14,9 @@ ARCHITECTURE=$(dpkg --print-architecture)
 
 
 
-latest=$(wget -qO- -T1 -t1 https://gitlab.com/api/v4/projects/ProjectWARP%2Fwarp-go/releases | awk -F '"' '{for (i=0; i<NF; i++) if ($i=="tag_name") {print $(i+2); exit}}' | sed "s/v//")
+latest=$(curl --connect-timeout 10 -q https://gitlab.com/api/v4/projects/ProjectWARP%2Fwarp-go/releases | awk -F '"' '{for (i=0; i<NF; i++) if ($i=="tag_name") {print $(i+2); exit}}' | sed "s/v//")
 latest=${latest:-'1.0.8'}
-wget --no-check-certificate -qO /tmp/warp-go.tar.gz https://raw.githubusercontent.com/fscarmen/warp/main/warp-go/warp-go_"$latest"_linux_"$ARCHITECTURE".tar.gz
+curl --connect-timeout 10 -o /tmp/warp-go.tar.gz https://raw.githubusercontent.com/fscarmen/warp/main/warp-go/warp-go_"$latest"_linux_"$ARCHITECTURE".tar.gz
 tar xzf /tmp/warp-go.tar.gz -C /tmp/ warp-go
 chmod +x /tmp/warp-go
 rm -f /tmp/warp-go.tar.gz
