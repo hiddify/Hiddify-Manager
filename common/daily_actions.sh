26cca019ea4bbb158e66a83b748c861ef82b7b4d
hiddify <hiddify@gmail.com>
2023-06-13 07:02:01 +0200

    remove: daily actions

--- common/daily_actions.sh
+++ common/daily_actions.sh
@@ -4,5 +4,5 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 cd $( dirname -- "$0"; )
 
 
-systemctl restart systemd-journald
+# systemctl restart systemd-journald
 sysctl -w vm.drop_caches=3
