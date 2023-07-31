e1a5d9a94e1c328ed87cb4ef0baf3cd6c92d05e1
hiddify <hiddify@gmail.com>
2023-07-31 22:11:58 +0200

    new: add ssh liberty bridge

---
+++ other/ssh/install.sh
@@ -0,0 +1,6 @@
+
+
+wget https://github.com/hiddify/ssh-liberty-bridge/releases/latest/download/ssh-liberty-bridge-$(dpkg --print-architecture)
+
+mv ssh-liberty-bridge-* ssh-liberty-bridge
+chmod +x ssh-liberty-bridge
