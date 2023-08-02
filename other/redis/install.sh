1303f1ae506957c27ae3557f70b39353764f215a
hiddify <hiddify@gmail.com>
2023-08-03 00:35:42 +0200

    fix: bug in redis installation

--- other/redis/install.sh
+++ other/redis/install.sh
@@ -5,4 +5,7 @@ sudo add-apt-repository -y universe
 sudo apt install -y redis-server
 systemctl disable redis-server
 ln -sf $(pwd)/hiddify-redis.service /etc/systemd/system/hiddify-redis.service
+touch /opt/hiddify-config/log/system/redis-server.log
+chown redis:redis /opt/hiddify-config/log/system/redis-server.log
 systemctl enable hiddify-redis
+systemctl start hiddify-redis
