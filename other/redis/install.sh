7be394c8e7c4b7bc5d06b0e077e04f08a3061332
hiddify <hiddify@gmail.com>
2023-08-03 08:45:55 +0200

    fix: the permission issue  in redis

--- other/redis/install.sh
+++ other/redis/install.sh
@@ -1,11 +1,15 @@
-sudo add-apt-repository -y universe
-# sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 
 
-sudo apt install -y redis-server
-systemctl disable redis-server
+
+if [[ ! command -v redis-server ]];then
+    sudo add-apt-repository -y universe
+    # sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+    sudo apt install -y redis-server
+fi
+systemctl disable --now redis-server
 ln -sf $(pwd)/hiddify-redis.service /etc/systemd/system/hiddify-redis.service
 touch /opt/hiddify-config/log/system/redis-server.log
 chown redis:redis /opt/hiddify-config/log/system/redis-server.log
+chown redis:redis /opt/hiddify-config/other/redis
 systemctl enable hiddify-redis
-systemctl start hiddify-redis
+systemctl restart hiddify-redis
---
+++ other/redis/run.sh
@@ -0,0 +1,1 @@
+systemctl start hiddify-redis
