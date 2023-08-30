3f06296bb2ad7724fa72147d099b1f0552abc174
hiddify <hiddify@gmail.com>
2023-08-30 15:37:22 +0200

    fix: install mysql

--- other/mysql/install.sh
+++ other/mysql/install.sh
@@ -2,14 +2,14 @@
 
 if ! command -v mysql; then
     sudo apt install -y mariadb-server
-
-    if [ ! -f "$password_file" ]; then
-        echo "Generating a random password..."
-        random_password=$(openssl rand -base64 40)
-        echo "$random_password" >"mysql_pass"
-        chmod 600 "mysql_pass"
-        # Secure MariaDB installation
-        sudo mysql_secure_installation <<EOF
+fi
+if [ ! -f "$password_file" ]; then
+    echo "Generating a random password..."
+    random_password=$(openssl rand -base64 40)
+    echo "$random_password" >"mysql_pass"
+    chmod 600 "mysql_pass"
+    # Secure MariaDB installation
+    sudo mysql_secure_installation <<EOF
 y
 $random_password
 $random_password
@@ -19,12 +19,12 @@ y
 y
 EOF
 
-        # Disable external access
-        sudo sed -i 's/bind-address/#bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
-        sudo systemctl restart mariadb
+    # Disable external access
+    sudo sed -i 's/bind-address/#bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
+    sudo systemctl restart mariadb
 
-        # Create user with localhost access
-        sudo mysql -u root <<MYSQL_SCRIPT
+    # Create user with localhost access
+    sudo mysql -u root <<MYSQL_SCRIPT
 CREATE USER 'hiddifypanel'@'localhost' IDENTIFIED BY '$random_password';
 GRANT ALL PRIVILEGES ON *.* TO 'hiddifypanel'@'localhost';
 CREATE DATABASE hiddifypanel;
@@ -32,8 +32,6 @@ GRANT ALL PRIVILEGES ON hiddifypanel.* TO 'hiddifypanel'@'localhost';
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
-        echo "MariaDB setup complete."
-
-    fi
+    echo "MariaDB setup complete."
 
 fi
