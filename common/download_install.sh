edc49252a40eaa69d3cf8937c080b851face4b01
hiddify <hiddify@gmail.com>
2023-09-09 12:07:47 +0200

    fix: multiple menu exit need

--- common/download_install.sh
+++ common/download_install.sh
@@ -15,24 +15,25 @@ GITHUB_USER=hiddify
 GITHUB_BRANCH_OR_TAG=main
 
 # if [ ! -d "/opt/$GITHUB_REPOSITORY" ];then
-        apt update
-        apt upgrade -y
-        apt install -y curl unzip
-        # pip3 install lastversion "requests<=2.29.0"
-        # pip install lastversion "requests<=2.29.0"
-        mkdir -p /opt/$GITHUB_REPOSITORY
-        cd /opt/$GITHUB_REPOSITORY
-        curl -L -o hiddify-config.zip https://github.com/hiddify/hiddify-config/releases/latest/download/hiddify-config.zip
-        unzip -o hiddify-config.zip
-        rm hiddify-config.zip
-        
-        bash install.sh
-        # exit 0
-# fi 
+apt update
+apt upgrade -y
+apt install -y curl unzip
+# pip3 install lastversion "requests<=2.29.0"
+# pip install lastversion "requests<=2.29.0"
+mkdir -p /opt/$GITHUB_REPOSITORY
+cd /opt/$GITHUB_REPOSITORY
+curl -L -o hiddify-config.zip https://github.com/hiddify/hiddify-config/releases/latest/download/hiddify-config.zip
+unzip -o hiddify-config.zip
+rm hiddify-config.zip
 
+bash install.sh
+# exit 0
+# fi
 
-echo "/opt/hiddify-config/menu.sh">>~/.bashrc
-echo "cd /opt/hiddify-config/">>~/.bashrc
+sed -i "s|/opt/hiddify-config/menu.sh||g" ~/.bashrc
+sed -i "s|cd /opt/hiddify-config/||g" ~/.bashrc
+echo "/opt/hiddify-config/menu.sh" >>~/.bashrc
+echo "cd /opt/hiddify-config/" >>~/.bashrc
 
 read -p "Press any key to go  to menu" -n 1 key
 cd /opt/$GITHUB_REPOSITORY
