b7938f750115c382421410da7b3c7db661b894f0
hiddify <hiddify@gmail.com>
2023-09-13 11:28:09 +0200

    fix: downgrade xray bug

--- common/downgrade.sh
+++ common/downgrade.sh
@@ -1,6 +1,7 @@
 cd /opt/hiddify-config/hiddify-panel
 python3 -m hiddifypanel downgrade
 cd ..
+rm xray/configs/
 pip install hiddifypanel==7.2.0
 curl -L -o hiddify-config.zip https://github.com/hiddify/hiddify-config/releases/download/v10.1.3/hiddify-config.zip
 unzip -o hiddify-config.zip
