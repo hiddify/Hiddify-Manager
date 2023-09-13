6c6eaebd5ba1f9dd81bf56759923c39ec9c7c468
hiddify <hiddify@gmail.com>
2023-09-13 14:50:29 +0200

    fix: firewall bugs

--- common/run.sh
+++ common/run.sh
@@ -1,76 +1,109 @@
 function add2iptables() {
   iptables -C $1 || echo "adding rule $1" && iptables -I $1
-  add2ip6tables "$1"
 }
 
 function add2ip6tables() {
   ip6tables -C $1 || echo "adding rule $1" && ip6tables -I $1
 }
+function allow_port() { #allow_port "tcp" "80"
+  add2iptables "INPUT -p $1 --dport $2 -j ACCEPT"
+  add2ip6tables "INPUT -p $1 --dport $2 -j ACCEPT"
+  # if [[ $1 == 'udp' ]]; then
+    add2iptables "INPUT -p $1 -m $1 --dport $2 -m conntrack --ctstate NEW -j ACCEPT"
+    add2ip6tables "INPUT -p $1 -m $1 --dport $2 -m conntrack --ctstate NEW -j ACCEPT"
+  # fi
+}
+
+function block_port() { #allow_port "tcp" "80"
+  add2iptables "INPUT -p $1 --dport $2 -j DROP"
+  add2ip6tables "INPUT -p $1 --dport $2 -j DROP"
+}
+
+function remove_port() { #allow_port "tcp" "80"
+  iptables -D INPUT -p $1 --dport $2
+  ip6tables -D INPUT -p $1 --dport $2
+}
 
 function allow_apps_ports() {
   service_name=$1
+  ports=$(ss -tulpn | grep "$service_name" | awk '{print $5}' | cut -d':' -f2)
 
-  port=$(ss -tulpn | grep "$service_name" | awk '{print $5}' | cut -d':' -f2)
-  if [[ -z $port ]]; then
+  if [[ -z $ports ]]; then
     echo "Service not found or not running"
   else
-    pid=$(ss -tulpn | grep "$service_name" | awk '{print $5}' | cut -d':' -f2 | sed 's/)//')
     path=$(ps -aux | grep $service_name | awk '{print $11}')
-    for p in $port; do
+
+    IFS=' ' read -ra portArray <<<"$ports"
+    for p in "${portArray[@]}"; do
       echo "Service is running on port $p and path $path"
-      add2iptables "INPUT -p tcp --dport $port -j ACCEPT"
-      # if [[ $1 == 'sshd' ]];then
-      #   add2iptables "INPUT -p tcp -m tcp --dport $PORT -m conntrack --ctstate NEW -j SSHBRUTE"
-      #   add2ip6tables "INPUT -p tcp -m tcp --dport $PORT -m conntrack --ctstate NEW -j SSHBRUTE"
-      # else
-      # add2iptables "INPUT -p tcp -m tcp --dport $PORT -m conntrack --ctstate NEW -j ACCEPT"
-      # add2ip6tables "INPUT -p tcp -m tcp --dport $PORT -m conntrack --ctstate NEW -j ACCEPT"
-      # fi
+      allow_port "tcp" $p
     done
   fi
 }
 
 mkdir -p /etc/iptables/
 
-add2iptables "INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT"
-add2iptables "INPUT -i lo -j ACCEPT"
-add2iptables "INPUT -p tcp --dport 443 -j ACCEPT"
-# add2iptables "INPUT -p udp --dport 443 -j ACCEPT"
-# add2iptables "INPUT -p udp --dport 3478 -j ACCEPT"
-if [[ $ssh_server_enable == 'true' ]]; then
-  add2iptables "INPUT -p tcp --dport ${ssh_server_port} -j ACCEPT"
-else
-  iptables -D INPUT -p tcp --dport ${ssh_server_port}
-fi
-add2iptables "INPUT -p udp --dport 53 -j ACCEPT"
-add2iptables "INPUT -p tcp --dport 53 -j ACCEPT"
-add2iptables "INPUT -p tcp --dport 80 -j ACCEPT"
-add2iptables "INPUT -p tcp --dport 22 -j ACCEPT"
-add2iptables "INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
-ip6tables -A INPUT -p icmpv6 --icmpv6-type 128 -j ACCEPT
 
+add2iptables "INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT"
+add2ip6tables "INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT"
+
+add2iptables "INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
+add2ip6tables "INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
+
+allow_port "tcp" 22
+allow_port "tcp" 80
+allow_port "tcp" 443
+allow_port "udp" 443
+allow_port "udp" 53
+allow_port "tcp" 53
+# allow_port "udp" 3478
+
+add2iptables "OUTPUT -p udp -j ACCEPT" 
+add2ip6tables "OUTPUT -p udp -j ACCEPT"
+
+add2iptables "OUTPUT -p tcp -j ACCEPT"
+add2ip6tables "OUTPUT -p tcp -j ACCEPT"
+
+add2iptables "INPUT -i lo -j ACCEPT"
+add2ip6tables "INPUT -i lo -j ACCEPT"
+
+
+# ICMP for ipv4
 add2iptables "INPUT -p icmp -m icmp --icmp-type 0 -m conntrack --ctstate NEW -j ACCEPT"
 add2iptables "INPUT -p icmp -m icmp --icmp-type 3 -m conntrack --ctstate NEW -j ACCEPT"
 add2iptables "INPUT -p icmp -m icmp --icmp-type 11 -m conntrack --ctstate NEW -j ACCEPT"
 add2iptables "INPUT -p icmp -m icmp --icmp-type 12 -m conntrack --ctstate NEW -j ACCEPT"
-add2iptables "INPUT -p tcp -m tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT"
-add2iptables "INPUT -p tcp -m tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT"
+
+# ICMP for ipv6
+add2ip6tables "INPUT -p ipv6-icmp --icmpv6-type 128 -m conntrack --ctstate NEW -j ACCEPT"
+add2ip6tables "INPUT -p ipv6-icmp --icmpv6-type 129 -m conntrack --ctstate NEW -j ACCEPT"
+add2ip6tables "INPUT -p ipv6-icmp --icmpv6-type 1 -m conntrack --ctstate NEW -j ACCEPT"
+add2ip6tables "INPUT -p ipv6-icmp --icmpv6-type 4 -m conntrack --ctstate NEW -j ACCEPT"
+add2ip6tables "INPUT -p ipv6-icmp --icmpv6-type 2 -m conntrack --ctstate NEW -j ACCEPT"
+
 
 allow_apps_ports "sshd"
 allow_apps_ports "x-ui"
-# allow_apps_ports "ssh-liberty-bridge"
-for PORT in ${HTTP_PORTS//,/ }; do
-  add2iptables "INPUT -p tcp --dport $PORT -j ACCEPT"
-done
-for PORT in ${TLS_PORTS//,/ }; do
-  add2iptables "INPUT -p tcp --dport $PORT -j ACCEPT"
+
+
+
+# Check if SSH server should be enabled
+if [[ $ssh_server_enable == 'true' ]]; then
+  allow_port "tcp" "$ssh_server_port"
+else
+  remove_port "tcp" "$ssh_server_port"  
+fi
+
+
+TCP_PORTS=(${HTTP_PORTS//,/ } ${TLS_PORTS//,/ })
+for PORT in "${TCP_PORTS[@]}"; do
+  allow_port $PORT
 done
 
-mkdir -p /etc/iptables/
 if [[ $ENABLE_FIREWALL == true ]]; then
   iptables -P INPUT DROP
-  # ip6tables -P INPUT DROP
-  ip6tables -P INPUT ACCEPT
+  ip6tables -P INPUT DROP
+  # ip6tables -P INPUT ACCEPT
 else
   iptables -P INPUT ACCEPT
   ip6tables -P INPUT ACCEPT
@@ -82,7 +115,6 @@ awk -i inplace '!seen[$0]++' /etc/iptables/rules.v6
 ip6tables-restore </etc/iptables/rules.v6
 iptables-restore </etc/iptables/rules.v4
 
-awk -i inplace '!seen[$0]++' /etc/iptables/rules.v6
 #add2iptables "INPUT -p tcp --dport 9000 -j DROP"
 
 if [[ $ENABLE_AUTO_UPDATE == true ]]; then
@@ -92,4 +124,3 @@ else
   rm -rf /etc/cron.d/hiddify_auto_update
   service cron reload
 fi
-
