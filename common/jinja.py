64e4991dfee377e106260bb6911ff191908ba344
hiddify <hiddify@gmail.com>
2023-10-08 18:33:11 +0200

    fix: bugs

--- common/jinja.py
+++ common/jinja.py
@@ -24,6 +24,7 @@ def render_j2_templates(start_path):
     for root, dirs, files in os.walk(start_path):
         for file in files:
             if file.endswith('.j2'):
+                print("Rendering: " + file)
                 # Create a template object by reading the file
                 template_path = os.path.join(root, file)
                 template = env.get_template(template_path)
@@ -35,8 +36,11 @@ def render_j2_templates(start_path):
                 output_file_path = os.path.splitext(template_path)[0]
                 if output_file_path.endswith('.json'):
                     # remove trailing comma and comments from json
-                    json5object = json5.loads(rendered_content)
-                    rendered_content = json5.dumps(json5object, trailing_commas=False, indent=2, quote_keys=True)
+                    try:
+                        json5object = json5.loads(rendered_content)
+                        rendered_content = json5.dumps(json5object, trailing_commas=False, indent=2, quote_keys=True)
+                    except Exception as e:
+                        print(f"Error parsing json: {e}")
 
                 with open(output_file_path, 'w') as output_file:
                     output_file.write(rendered_content)
