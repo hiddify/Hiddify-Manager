c9646e2782dfb716588758639aaab54cc4612a88
hiddify <hiddify@gmail.com>
2023-10-19 09:45:19 +0200

    fix: bug

--- acme.sh/run.sh
+++ acme.sh/run.sh
@@ -1,4 +1,4 @@
-domains=$(cat ../current.json | jq '.domains[] | select(.mode | IN("direct", "cdn", "worker", "relay", "auto_cdn_ip", "old_xtls_direct", "sub_link_only")) | .domain')
+domains=$(cat ../current.json | jq -r '.domains[] | select(.mode | IN("direct", "cdn", "worker", "relay", "auto_cdn_ip", "old_xtls_direct", "sub_link_only")) | .domain')
 
 for d in $domains; do
 	bash get_cert.sh $d
