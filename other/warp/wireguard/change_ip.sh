28a201699438805a624d79189f3743a7ab146555
lymanjre <125398461+lymanjre@users.noreply.github.com>
2023-11-20 12:24:00 +0330

    Update change_ip.sh
    Change directory to the location of WARP files

--- other/warp/wireguard/change_ip.sh
+++ other/warp/wireguard/change_ip.sh
@@ -2,7 +2,7 @@
 
 wget -N https://raw.githubusercontent.com/fscarmen/warp/main/api.sh && bash api.sh -u -f wgcf-account.toml
 # Change directory to the location of WARP files
-cd /opt/hiddify-manager/other/warp/wireguard
+cd $(dirname --"$0")
 mv wgcf-account.toml wgcf-account.toml.backup
 # curl --connect-timeout 1 -Lo wgcf.zip https://api.zeroteam.top/warp?format=wgcf
 # unzip -o wgcf.zip
