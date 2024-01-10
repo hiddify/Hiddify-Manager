a5b49fdc98bcabad720a3b67efa5e570b2e7d696
hiddify <hiddify@gmail.com>
2024-01-11 00:03:14 +0100

    fix: bug in lastversion

--- common/utils.sh
+++ common/utils.sh
@@ -9,15 +9,16 @@ function get_commit_version() {
 }
 
 function get_pre_release_version() {
-    lastversion "$1" --pre --at github
+    # lastversion "$1" --pre --at github
+    curl -sL "https://api.github.com/repos/hiddify/$1/releases" | jq -r 'map(select(.prerelease == false or .draft == true)) | sort_by(.created_at) | last | .tag_name'
 }
 
 function get_release_version() {
     # COMMIT_URL=https://api.github.com/repos/hiddify/$1/releases/latest
     # VERSION=$(curl -s --connect-timeout 1 $COMMIT_URL | jq -r .tag_name)
-    location=$(curl -sI "https://github.com/hiddify/$1/releases/latest" | grep -i location| awk -F' ' '{print $2}' | tr -d '\r')
+    location=$(curl -sI "https://github.com/hiddify/$1/releases/latest" | grep -i location | awk -F' ' '{print $2}' | tr -d '\r')
     if [[ $location == *"latest"* ]]; then
-        location=$(curl -sI "$location" | grep -i location| awk -F' ' '{print $2}' | tr -d '\r')
+        location=$(curl -sI "$location" | grep -i location | awk -F' ' '{print $2}' | tr -d '\r')
     fi
 
     VERSION=$(echo $location | rev | awk -F/ '{print $1}' | rev)
