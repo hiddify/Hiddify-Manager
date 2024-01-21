1105bad1a3c352859e5ba6bb9526332c8ffbcf01
hiddify <hiddify@gmail.com>
2024-01-21 21:21:59 +0100

    better logs

--- other/telegram/tgo/install.sh
+++ other/telegram/tgo/install.sh
@@ -1,6 +1,6 @@
 echo "telegram proxy install.sh $*"
-systemctl kill mtproxy.service
-systemctl disable mtproxy.service
+systemctl kill mtproxy.service >/dev/null 2>&1
+systemctl disable mtproxy.service >/dev/null 2>&1
 
 # sudo add-apt-repository -y ppa:longsleep/golang-backports
 # sudo apt update
