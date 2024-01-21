0756cdc987ed0ba5d7678d7439afe9a9a6906c89
hiddify <hiddify@gmail.com>
2024-01-21 20:46:32 +0100

    silent get

--- common/download.sh
+++ common/download.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $1 == "release" ]; then
-    sudo bash -c "$(curl -Lfo- https://raw.githubusercontent.com/hiddify/hiddify-config/main/common/download_install.sh)"
+    sudo bash -c "$(curl -sLfo- https://raw.githubusercontent.com/hiddify/hiddify-config/main/common/download_install.sh)"
     exit $?
 fi
 
 mkdir -p /tmp/hiddify/
-curl -L -o /tmp/hiddify/hiddify_installer.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/hiddify_installer.sh
-curl -L -o /tmp/hiddify/utils.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/utils.sh
+curl -sL -o /tmp/hiddify/hiddify_installer.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/hiddify_installer.sh
+curl -sL -o /tmp/hiddify/utils.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/utils.sh
 bash /tmp/hiddify/hiddify_installer.sh $@
