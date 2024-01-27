e47d07f960a3c8eb89981be6585131ac670c0a02
hiddify <hiddify@gmail.com>
2024-01-27 11:13:06 +0100

    fix: bug

--- common/download.sh
+++ common/download.sh
@@ -10,4 +10,6 @@ fi
 mkdir -p /tmp/hiddify/
 curl -sL -o /tmp/hiddify/hiddify_installer.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/hiddify_installer.sh
 curl -sL -o /tmp/hiddify/utils.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/utils.sh
+chmod +x /tmp/hiddify/hiddify_installer.sh
+chmod +x /tmp/hiddify/utils.sh
 bash /tmp/hiddify/hiddify_installer.sh $@
