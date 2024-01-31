b15785c2488d3acd1b80a11b26e3bcb5817f48b1
Sarina <iam54r1n40@proton.me>
2024-01-31 18:44:57 +0330

    fix: erlang mtproto proxy

--- other/telegram/erlang/run.sh
+++
@@ -1,9 +0,0 @@
-# systemctl enable mtproxy.service
-# systemctl restart mtproxy.service
-
-cd mtproto_proxy
-sudo make update-sysconfig 
-
-sudo systemctl enable mtproto-proxy
-sudo systemctl restart mtproto-proxy
-systemctl status mtproto-proxy --no-pager
