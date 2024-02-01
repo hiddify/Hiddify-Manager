401926216e44cf9a57f8caceb0e6c039a5109e31
hiddify <hiddify@gmail.com>
2024-02-01 13:45:07 +0100

    update req

--- common/hiddify_installer.sh
+++ common/hiddify_installer.sh
@@ -43,7 +43,7 @@ function install_panel() {
         echo "cd /opt/hiddify-manager/" >>~/.bashrc
     fi
     install_python
-    install_package wireguard
+    install_package wireguard libev-dev libevdev2 default-libmysqlclient-dev build-essential pkg-config
     update_panel "$package_mode" "$force"
     panel_update=$?
     update_config "$package_mode" "$force"
