b8ea39949d70bccb35137a6ef32ae1f4c0ae34c8
hiddify-com <hiddify@gmail.com>
2024-03-04 02:33:24 +0330

    fix:

--- common/download.sh
+++ common/download.sh
@@ -12,4 +12,4 @@ curl -sL -o /tmp/hiddify/hiddify_installer.sh https://raw.githubusercontent.com/
 curl -sL -o /tmp/hiddify/utils.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/utils.sh
 chmod +x /tmp/hiddify/hiddify_installer.sh
 chmod +x /tmp/hiddify/utils.sh
-bash /tmp/hiddify/hiddify_installer.sh $@
+/tmp/hiddify/hiddify_installer.sh $@
