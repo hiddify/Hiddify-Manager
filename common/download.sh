54433fe52854892ad4b558d22128f1750e883616
hiddify-com <hiddify@gmail.com>
2024-03-04 03:48:56 +0330

    fix: bug

--- common/download.sh
+++ common/download.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [[ "$VER" != "" ]];then
-    set -- $VER 
-fi
 
 
 
@@ -14,9 +11,9 @@ if [[ " $@ " != "--no-gui" && "$0" == "bash" ]]; then
     exit 1
 fi
 
-echo "Downloading $@"
+echo "Downloading '$@'"
 
-if [[ " $@ " == *" release "* || " $@ " == *" v8 "* ]]; then
+if [[ " $VER $@ " == *" release "* || " $VER $@ " == *" v8 "* ]]; then
     sudo bash -c "$(curl -sLfo- https://raw.githubusercontent.com/hiddify/hiddify-config/main/common/download_install.sh)"
     exit $?
 fi
@@ -26,4 +23,4 @@ curl -sL -o /tmp/hiddify/hiddify_installer.sh https://raw.githubusercontent.com/
 curl -sL -o /tmp/hiddify/utils.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/utils.sh
 chmod +x /tmp/hiddify/hiddify_installer.sh
 chmod +x /tmp/hiddify/utils.sh
-/tmp/hiddify/hiddify_installer.sh $@
+/tmp/hiddify/hiddify_installer.sh $VER $@
