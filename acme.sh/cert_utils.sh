983e769784997cb2da47e620f07791ad65da59d0
hiddify-com <hiddify@gmail.com>
2024-03-07 21:33:03 +0330

    fix: self signed cert of long domains

--- acme.sh/cert_utils.sh
+++ acme.sh/cert_utils.sh
@@ -83,7 +83,7 @@ function get_self_signed_cert() {
     d=$1
     if [ ${#d} -gt 64 ]; then
         echo "Domain length exceeds 64 characters. Truncating to the first 64 characters."
-        d="${d:0:64}"
+        get_self_signed_cert "${d:0:64}"
     fi
     mkdir -p ../ssl
     certificate="../ssl/$d.crt"
