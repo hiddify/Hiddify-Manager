54bf57976ed2de8f937027593764ae1ea470c8ef
Sarina <iam54r1n40@proton.me>
2024-03-10 08:57:28 +0330

    add: more services to restart script

--- restart.sh
+++ restart.sh
@@ -9,15 +9,17 @@ function main() {
 	after_stats=""
 
 	# Restart services and get their status (except hiddify-panel)
-    for s in **/*.service nginx; do
+    for s in other/**/*.service **/*.service wg-quick@warp mtproto-proxy.service mtproxy.service;do
         s=${s##*/}
         s=${s%%.*}
 		if [ "$s" == "hiddify-panel" ]; then
 			continue;
 		fi
-		before_stats+=$(printf "%-30s %-30s %30s" $s $(get_pretty_service_status $s 2>&1) "\n")
-		systemctl restart "$s"
-		after_stats+=$(printf "%-30s %-30s %30s" $s $(get_pretty_service_status $s 2>&1) "\n")
+		if systemctl is-enabled $s >/dev/null 2>&1 ; then
+			before_stats+=$(printf "%-30s %-30s %30s" $s $(get_pretty_service_status $s 2>&1) "\n")
+			systemctl restart "$s"
+			after_stats+=$(printf "%-30s %-30s %30s" $s $(get_pretty_service_status $s 2>&1) "\n")
+		fi
     done
 	
 	# Restart hiddify-panel separately from others
