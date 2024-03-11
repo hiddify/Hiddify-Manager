040850223ec84c8ab49e473fcd38021d30395d7e
Sarina <iam54r1n40@proton.me>
2024-03-12 02:43:17 +0330

    add: config file

--- other/testers/uninstall.sh
+++ other/testers/uninstall.sh
@@ -2,6 +2,3 @@
 rm -rf SUB_LINK HiddifyCli webui clash.db tmp VERSION
 systemctl stop hiddify-cli.service > /dev/null 2>&1
 systemctl disable hiddify-cli.service > /dev/null 2>&1
-pkill HiddifyCli
-fuser -k 6756/tcp
-fuser -k 2334/tcp
