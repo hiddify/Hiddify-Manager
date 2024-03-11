c42f3516c70d199ae532f3e5a5d1affb849d45ea
Sarina <iam54r1n40@proton.me>
2024-03-11 03:48:26 +0330

    add: hiddify-next-core(Hiddify Cli)

---
+++ other/testers/install.sh
@@ -0,0 +1,16 @@
+source ../../common/utils.sh
+
+latest=$(get_release_version hiddify-next-core)
+if [ "$(cat 'VERSION' 2>/dev/null)" != "$latest" ] || ! is_installed ./HiddifyCli; then
+    echo "Downloading HiddifyCli..."
+    pkg=$(dpkg --print-architecture)
+
+    curl -Lo hiddify-cli.tar.gz https://github.com/hiddify/hiddify-next-core/releases/download/v$latest/hiddify-cli-linux-$pkg.tar.gz
+
+    tar -xzf hiddify-cli.tar.gz
+
+    echo $latest> VERSION
+    echo "The HiddifyCli installed"
+else
+    echo "HiddifyCli is installed"
+fi
