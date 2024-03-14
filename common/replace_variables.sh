2017227903a09e62adad47772ecc206554d4b575
hiddify-com <hiddify@gmail.com>
2024-03-14 03:54:37 +0330

    fix: xray issue

--- common/replace_variables.sh
+++ common/replace_variables.sh
@@ -1,4 +1,4 @@
 python -c "import json5;import jinja2" || pip install json5 jinja2
-rm -f /opt/hiddify-manager/singbox/configs/*.json
-rm -f /opt/hiddify-manager/xray/configs/*.json
+# rm -f /opt/hiddify-manager/singbox/configs/*.json
+# rm -f /opt/hiddify-manager/xray/configs/*.json
 python3 /opt/hiddify-manager/common/jinja.py $MODE
