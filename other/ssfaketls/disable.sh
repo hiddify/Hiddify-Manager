3eefcc88acabf21fc223d470347740d83b3fbb81
Hiddify <114227601+hiddify-com@users.noreply.github.com>
2024-06-22 18:54:25 +0200

    Update disable.sh

--- other/ssfaketls/disable.sh
+++ other/ssfaketls/disable.sh
@@ -1,5 +1,8 @@
 systemctl stop hiddify-ss-v2ray.service > /dev/null 2>&1
 systemctl stop hiddify-ss-faketls.service > /dev/null 2>&1
 
-systemctl disable hiddify-ss-v2ray.service > /dev/null 2>&1
-systemctl disable hiddify-ss-faketls.service > /dev/null 2>&1
+systemctl disable --now ss-v2ray.service > /dev/null 2>&1
+systemctl disable --now ss-faketls.service > /dev/null 2>&1
+
+systemctl disable --now hiddify-ss-v2ray.service > /dev/null 2>&1
+systemctl disable --now hiddify-ss-faketls.service > /dev/null 2>&1
