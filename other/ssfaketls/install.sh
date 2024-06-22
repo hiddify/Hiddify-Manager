3b5e7d5a28adf96f7cc7a68dba710fc58d2e4f94
Hiddify <114227601+hiddify-com@users.noreply.github.com>
2024-06-22 18:53:32 +0200

    Update install.sh

--- other/ssfaketls/install.sh
+++ other/ssfaketls/install.sh
@@ -3,3 +3,4 @@ source /opt/hiddify-manager/common/utils.sh
 install_package shadowsocks-libev simple-obfs
 
 ln -sf $(pwd)/hiddify-ss-faketls.service /etc/systemd/system/hiddify-ss-faketls.service
+systemctl disable --now ss-faketls.service
