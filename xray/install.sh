cce384edaaa80ca77667d30205d31b84a2c0149a
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-06-26 20:16:00 +0330

    update xray to 1.8.16

--- xray/install.sh
+++ xray/install.sh
@@ -1,6 +1,6 @@
 source ../common/utils.sh
 # latest= #$(get_release_version hiddify-sing-box)
-latest=1.8.10
+latest=1.8.16
 mkdir -p bin
 if [ "$(cat VERSION 2>/dev/null)" != "$latest" ] || ! is_installed ./bin/xray; then
     pkg=$(dpkg --print-architecture)
