91b78233c11f17b4c714a084414dcccc5894b987
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-07-04 23:54:05 +0330

    remove old domains from crts

--- haproxy/run.sh
+++ haproxy/run.sh
@@ -7,6 +7,20 @@
 source ../common/utils.sh
 domains=$(cat ../current.json | jq -r '.domains[] | select(.mode | IN("direct", "cdn", "worker", "relay", "auto_cdn_ip", "old_xtls_direct", "sub_link_only", "fake")) | .domain')
 
+
+# Loop over the .crt files
+for f in ../ssl/*.crt; do
+    # Get the basename without the .crt extension
+    d=$(basename "$f" .crt)
+    
+    # Check if $d is not in the list of domains
+    if [[ ! " ${domains[@]} " =~ " ${d} " ]]; then
+        # If $d is not in domains, remove the file
+        rm "../ssl/$d.crt"
+        rm "../ssl/$d.crt.key"
+    fi
+done
+
 # we need at least one ssl certificate to be able to run haproxy
 for d in $domains; do
     bash ../acme.sh/generate_self_signed_cert.sh $d
