d0895ba221f647caac76a6b17e43de79e8098782
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-07-28 23:33:32 +0330

    fix: fakesni bugs

--- other/ssfaketls/disable.sh
+++ other/ssfaketls/disable.sh
@@ -6,3 +6,4 @@ systemctl disable --now ss-faketls.service > /dev/null 2>&1
 
 systemctl disable --now hiddify-ss-v2ray.service > /dev/null 2>&1
 systemctl disable --now hiddify-ss-faketls.service > /dev/null 2>&1
+rm ss-faketls.service*
--- other/ssfaketls/install.sh
+++ other/ssfaketls/install.sh
@@ -4,3 +4,4 @@ install_package shadowsocks-libev simple-obfs
 
 ln -sf $(pwd)/hiddify-ss-faketls.service /etc/systemd/system/hiddify-ss-faketls.service
 systemctl disable --now ss-faketls.service
+rm ss-faketls.service*
