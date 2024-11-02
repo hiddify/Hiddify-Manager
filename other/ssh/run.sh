ddaab9c54b0cf5259cdcb0981e8e9a99dac28269
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-11-03 01:09:16 +0330

    new: add seperate REDIS config for main and ssh usage

--- other/ssh/run.sh
+++ other/ssh/run.sh
@@ -4,6 +4,11 @@
 ln -sf $(pwd)/hiddify-ssh-liberty-bridge.service /etc/systemd/system/hiddify-ssh-liberty-bridge.service
 
 chown -R liberty-bridge host_key
+sed -i '/REDIS_URI/d' .env
+REDIS_PASS=$(grep '^requirepass' "../redis/redis.conf" | awk '{print $2}')
+echo "REDIS_URI = 'redis://:$REDIS_PASS@127.0.0.1:6379/1'" >>.env
+
+
 chmod 600 .env*
 
 systemctl enable hiddify-ssh-liberty-bridge
