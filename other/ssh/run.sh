61434a042c9c5c67f45b637155945657fef99205
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-11-04 00:50:12 +0330

    fix: ssh liberty plus

--- other/ssh/run.sh
+++ other/ssh/run.sh
@@ -6,7 +6,7 @@ ln -sf $(pwd)/hiddify-ssh-liberty-bridge.service /etc/systemd/system/hiddify-ssh
 chown -R liberty-bridge host_key
 sed -i '/REDIS_URI/d' .env
 REDIS_PASS=$(grep '^requirepass' "../redis/redis.conf" | awk '{print $2}')
-echo "REDIS_URI = 'redis://:$REDIS_PASS@127.0.0.1:6379/1'" >>.env
+echo "REDIS_URI='redis://:$REDIS_PASS@127.0.0.1:6379/1'" >>.env
 
 
 chmod 600 .env*
