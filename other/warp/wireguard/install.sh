060b677acd6b1f57d89030b6b0da4c85b8b2caae
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-11-07 01:49:58 +0330

    fix: wiregaurd dl signture

--- other/warp/wireguard/install.sh
+++ other/warp/wireguard/install.sh
@@ -9,7 +9,7 @@ if ! is_installed ./wgcf; then
     pkg=$(dpkg --print-architecture)
     HASH=${hashes[$pkg]-:hashes[amd64]}
 
-    download_and_check_hash wgcf https://github.com/ViRb3/wgcf/releases/download/v2.2.23/wgcf_2.2.23_linux_$pkg || return $0
+    download_and_check_hash wgcf https://github.com/ViRb3/wgcf/releases/download/v2.2.23/wgcf_2.2.23_linux_$pkg $HASH|| return $0
     chmod +x wgcf
     
     
