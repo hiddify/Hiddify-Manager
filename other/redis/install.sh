7fc34d013c57e136b1b146690a0db2c4c74ad67d
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-11-18 03:15:12 +0330

    fix: bug

--- other/redis/install.sh
+++ other/redis/install.sh
@@ -3,18 +3,23 @@ if ! is_installed redis-server; then
     add-apt-repository -y universe
     install_package redis-server
 fi
+ln -sf $(pwd)/hiddify-redis.service /etc/systemd/system/hiddify-redis.service >/dev/null 2>&1
+systemctl enable hiddify-redis
+touch /opt/hiddify-manager/log/system/redis-server.log
+chown redis:redis /opt/hiddify-manager/log/system/redis-server.log
+chown -R redis:redis .
+chmod 600 redis.conf
+
 if ! grep -q "^requirepass" "redis.conf"; then
     # Generate a random password
     random_password=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | head -c49; echo)
     # Add requirepass with the generated password to redis.conf
     echo "requirepass $random_password" >>redis.conf   
+    systemctl disable --now redis-server >/dev/null 2>&1
+    pkill -9 redis-server
+    systemctl restart hiddify-redis
 fi
-systemctl disable --now redis-server >/dev/null 2>&1
-pkill -9 redis-server
-ln -sf $(pwd)/hiddify-redis.service /etc/systemd/system/hiddify-redis.service >/dev/null 2>&1
-touch /opt/hiddify-manager/log/system/redis-server.log
-chown redis:redis /opt/hiddify-manager/log/system/redis-server.log
-chown -R redis:redis /opt/hiddify-manager/other/redis
-chmod 600 redis.conf
-systemctl enable hiddify-redis
+
+
+
 # systemctl reload hiddify-redis
