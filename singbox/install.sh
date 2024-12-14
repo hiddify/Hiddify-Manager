37e0115ef1992edb0db6c1709ad9b1a882aef2cc
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-12-14 23:01:32 +0330

    fix: issue in incomplete installation

--- singbox/install.sh
+++ singbox/install.sh
@@ -5,14 +5,22 @@ rm -rf configs/*.template
 # latest= #$(get_release_version hiddify-sing-box)
 version="" #use specific version if needed otherwise it will use the latest
 download_package singbox sb.zip $version
-if [ "$?" == "0"  ] || ! is_installed ./sing-box; then
-    unzip -o sb.zip > /dev/null
-    cp -f sing-box-*/sing-box . 2>/dev/null
-    rm -r sb.zip sing-box-* 2>/dev/null
-    chown root:root sing-box
-    chmod +x sing-box
-    ln -sf /opt/hiddify-manager/singbox/sing-box /usr/bin/sing-box
-    rm geosite.db 2>/dev/null
 
-    set_installed_version singbox $version
+function install_singbox() {
+    install_package unzip 
+    unzip -o sb.zip > /dev/null || return 1
+    cp -f sing-box-*/sing-box . 2>/dev/null || return 2
+    rm -r sb.zip sing-box-* 2>/dev/null || return 3
+    chown root:root sing-box || return 4
+    chmod +x sing-box || return 5
+    ln -sf /opt/hiddify-manager/singbox/sing-box /usr/bin/sing-box || return 6
+    rm geosite.db 2>/dev/null 
+}
+if [ "$?" == "0"  ] || ! is_installed ./sing-box; then
+    if install_singbox; then
+        set_installed_version singbox $version
+    else
+        echo "Failed to install singbox $?"
+        exit 1    
+    fi
 fi
--- xray/install.sh
+++ xray/install.sh
@@ -4,14 +4,23 @@ source ../common/package_manager.sh
 version="" #use specific version if needed otherwise it will use the latest
 mkdir -p bin run
 download_package xray sb.zip $version
-if [ "$?" == "0"  ] || ! is_installed ./bin/xray; then
-    systemctl stop hiddify-xray.service > /dev/null 2>&1
+
+function install_xray() {
+    systemctl stop hiddify-xray.service > /dev/null 2>&1 
     rm -rf bin/*
-    install_package unzip
-    unzip -o sb.zip -d bin/ > /dev/null
-    rm -r sb.zip
-    chown root:root bin/xray
-    chmod +x bin/xray
-    ln -sf /opt/hiddify-manager/xray/bin/xray /usr/bin/xray
-    set_installed_version xray $version
+    install_package unzip 
+    unzip -o sb.zip -d bin/ > /dev/null || return 1
+    rm -r sb.zip 
+    chown root:root bin/xray || return 2
+    chmod +x bin/xray || return 3
+    ln -sf /opt/hiddify-manager/xray/bin/xray /usr/bin/xray || return 3
+}
+
+if [ "$?" == "0"  ] || ! is_installed ./bin/xray; then
+    if install_xray; then
+        set_installed_version xray $version
+    else
+        echo "Failed to install xray $?"
+        exit 1
+    fi
 fi
