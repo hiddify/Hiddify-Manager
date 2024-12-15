f08bd990c4e76371c0a49290683b586577b3acb2
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-12-15 18:35:45 +0330

    new: speed up cert by parallel req

--- acme.sh/run.sh
+++ acme.sh/run.sh
@@ -5,16 +5,21 @@ source ./cert_utils.sh
 domains=$(cat ../current.json | jq -r '.domains[] | select(.mode | IN("direct",   "relay", "old_xtls_direct", "sub_link_only")) | .domain')
 
 for d in $domains; do
-    get_cert $d
+    get_cert $d &
 done
+wait
 
 domains=$(cat ../current.json | jq -r '.domains[] | select(.mode | IN("fake")) | .domain')
 for d in $domains; do
-    get_self_signed_cert $d
+    get_self_signed_cert $d &
 done
+wait
 
 for f in ../ssl/*.crt; do
     d=$(basename "$f" .crt)
-    get_self_signed_cert $d
+    get_self_signed_cert $d &
 done
+wait
 systemctl reload hiddify-haproxy
+systemctl reload hiddify-singbox
+# systemctl reload hiddify-xray
