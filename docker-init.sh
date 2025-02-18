526117786341c382676154faf05680b580acddd7
Tesla <tes@Teslas-MacBook-Pro.local>
2025-02-18 17:24:19 +0330

    Update docker init no-gui

--- docker-init.sh
+++ docker-init.sh
@@ -3,7 +3,7 @@
 mkdir /hiddify-data/ssl/
 rm -rf /opt/hiddify-manager/log/*.lock
 cd $(dirname -- "$0")
-DO_NOT_INSTALL=true ./install.sh install-docker $@
+DO_NOT_INSTALL=true ./install.sh install-docker --no-gui $@
 ./status.sh --no-gui
 
 echo Hiddify is started!!!! in 5 seconds you will see the system logs
