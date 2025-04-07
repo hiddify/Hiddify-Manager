a2fe101737e1ac8a6c6ea78654193cadcd49f21c
Tesla <tes@Teslas-MacBook-Pro.local>
2025-04-08 00:32:57 +0330

    Update and adding apply_config for actication hiddify cli

--- common/hiddify_installer.sh
+++ common/hiddify_installer.sh
@@ -67,7 +67,7 @@ function install_panel() {
     if is_installed hiddifypanel && [[ -z "$package_mode" || ($package_mode == "develop" || $package_mode == "beta" || $package_mode == "release") ]]; then
         hiddify-panel-cli set-setting -k package_mode -v $1
     fi
-    bash /opt/hiddify-manager/apply_configs.sh
+    bash /opt/hiddify-manager/apply_configs.sh --no-gui --no-log
 }
 
 function update_panel() {
