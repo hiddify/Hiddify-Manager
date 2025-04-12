5299b8b76c5f0d0a02e73b3d2e53d7414f621cb7
tesla <tesilaaliset@gmail.com>
2025-04-13 01:09:23 +0330

    Fix installation of haproxy based on ubuntu version.

--- haproxy/install.sh
+++ haproxy/install.sh
@@ -7,9 +7,32 @@ if is_installed sniproxy; then
     pkill -9 sniproxy >/dev/null 2>&1
 fi
 
-if ! is_installed_package "haproxy=3.1"; then
-    add-apt-repository -y ppa:vbernat/haproxy-3.1
-    install_package haproxy=3.1.\*
+OS_VERSION=$(lsb_release -rs | cut -d'.' -f1)
+echo "Detected OS version: Ubuntu $OS_VERSION"
+
+if [ "$OS_VERSION" -eq 22 ]; then
+    echo "OS version is 22, checking for haproxy=3.0"
+    if ! is_installed_package "haproxy=3.0"; then
+        echo "Adding PPA for haproxy-3.0"
+        add-apt-repository -y ppa:vbernat/haproxy-3.0
+        echo "Installing haproxy 3.0"
+        install_package haproxy=3.0.*
+    else
+        echo "haproxy 3.0 is already installed"
+    fi
+elif [ "$OS_VERSION" -eq 24 ]; then
+    echo "OS version is 24, checking for haproxy=3.1"
+    if ! is_installed_package "haproxy=3.1"; then
+        echo "Adding PPA for haproxy-3.1"
+        add-apt-repository -y ppa:vbernat/haproxy-3.1
+        echo "Installing haproxy 3.1"
+        install_package haproxy=3.1.*
+    else
+        echo "haproxy 3.1 is already installed"
+    fi
+else
+    echo "OS version is neither 22 nor 24, skipping haproxy installation"
+    exit 1
 fi
 systemctl kill haproxy >/dev/null 2>&1
 systemctl stop haproxy >/dev/null 2>&1
