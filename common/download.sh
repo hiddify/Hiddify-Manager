d4f7837405f54ff2df32801f2e97ebbc0dd60f94
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2025-04-14 22:10:41 +0330

    fix: beta installer

--- common/download.sh
+++ common/download.sh
@@ -27,8 +27,14 @@ fi
 mkdir -p /tmp/hiddify/
 chmod 600 /tmp/hiddify/
 rm -rf /tmp/hiddify/*
-curl -sL -o /tmp/hiddify/hiddify_installer.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/hiddify_installer.sh
-curl -sL -o /tmp/hiddify/utils.sh https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/utils.sh
+
+if [ "$1" == "v*" ];then
+    base_url=https://raw.githubusercontent.com/hiddify/Hiddify-Manager/refs/tags/$1/
+else 
+    base_url=https://raw.githubusercontent.com/hiddify/Hiddify-Manager/refs/heads/$1/
+fi
+curl -sL -o /tmp/hiddify/hiddify_installer.sh $base_url/common/hiddify_installer.sh
+curl -sL -o /tmp/hiddify/utils.sh $base_url/common/utils.sh
 chmod 700 /tmp/hiddify/*
 
 /tmp/hiddify/hiddify_installer.sh $@
