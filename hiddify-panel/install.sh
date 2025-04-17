3a4c9e258c96c87e30db4d0554570bbe875167cd
tesla <tesilaaliset@gmail.com>
2025-04-17 18:17:44 +0330

    Pinning marshmellow version

--- hiddify-panel/install.sh
+++ hiddify-panel/install.sh
@@ -37,6 +37,9 @@ rm -rf /etc/cron.d/{hiddify_usage_update,hiddify_auto_backup}
 # echo "0 */6 * * * hiddify-panel $(pwd)/backup.sh" >/etc/cron.d/hiddify_auto_backup
 service cron reload >/dev/null 2>&1
 
+## TODO: Pinning marshmellow version, it will be removed and api_flask should pin it
+pip install marshmellow==3.26.1
+
 
 ##### download videos
 
