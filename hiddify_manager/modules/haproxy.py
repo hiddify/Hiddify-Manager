595981f912dcb966936a1a67ce912b31f4a28141
Mohammad <mohammad.rasim96@gmail.com>
2026-06-09 19:33:19 +0300

    feat: migrate nginx, haproxy, and acme.sh installation to python

---
+++ hiddify_manager/modules/haproxy.py
@@ -0,0 +1,28 @@
+import os
+from hiddify_manager.utils.logger import log
+from hiddify_manager.utils.shell import run_cmd
+import glob
+
+def install():
+    module_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "haproxy")
+    
+    for template in glob.glob(os.path.join(module_dir, "*.template")):
+        os.remove(template)
+        
+    run_cmd(["systemctl", "stop", "hiddify-sniproxy"], check=False)
+    run_cmd(["systemctl", "disable", "hiddify-sniproxy"], check=False)
+    run_cmd(["pkill", "-9", "sniproxy"], check=False)
+    
+    run_cmd(["add-apt-repository", "-y", "ppa:vbernat/haproxy-3.0"], check=False)
+    run_cmd(["apt-get", "update", "-y"], check=False)
+    run_cmd(["apt-get", "install", "-y", "haproxy"])
+    
+    run_cmd(["systemctl", "kill", "haproxy"], check=False)
+    run_cmd(["systemctl", "stop", "haproxy"], check=False)
+    run_cmd(["systemctl", "disable", "haproxy"], check=False)
+    
+    svc_file = os.path.join(module_dir, "hiddify-haproxy.service")
+    if os.path.exists(svc_file):
+        run_cmd(["ln", "-sf", svc_file, "/etc/systemd/system/hiddify-haproxy.service"])
+        run_cmd(["systemctl", "enable", "hiddify-haproxy.service"])
+    log.info("HAProxy setup complete.")
---
+++ hiddify_manager/modules/nginx.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+from hiddify_manager.utils.logger import log
+from hiddify_manager.utils.shell import run_cmd
+
+def install():
+    module_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "nginx")
+    
+    run_cmd(["useradd", "nginx"], check=False)
+    
+    run_cmd(["apt-get", "update", "-y"], check=False)
+    run_cmd(["apt-get", "install", "-y", "nginx"])
+    
+    services_to_kill = ["nginx", "apache2"]
+    for svc in services_to_kill:
+        run_cmd(["systemctl", "kill", svc], check=False)
+        run_cmd(["systemctl", "disable", svc], check=False)
+        
+    old_configs = [
+        "/etc/nginx/conf.d/web.conf",
+        "/etc/nginx/sites-available/default",
+        "/etc/nginx/sites-enabled/default",
+        "/etc/nginx/conf.d/default.conf",
+        "/etc/nginx/conf.d/xray-base.conf",
+        "/etc/nginx/conf.d/speedtest.conf"
+    ]
+    for cfg in old_configs:
+        if os.path.exists(cfg):
+            os.remove(cfg)
+            
+    os.makedirs(os.path.join(module_dir, "run"), exist_ok=True)
+    
+    hiddify_nginx_svc = os.path.join(module_dir, "hiddify-nginx.service")
+    if os.path.exists(hiddify_nginx_svc):
+        run_cmd(["ln", "-sf", hiddify_nginx_svc, "/etc/systemd/system/hiddify-nginx.service"])
+        run_cmd(["systemctl", "enable", "hiddify-nginx.service"])
+    log.info("Nginx setup complete.")
