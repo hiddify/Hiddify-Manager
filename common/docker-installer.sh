9c008a266c788c6b8aea8df190a11511218aad02
Hiddify <114227601+hiddify-com@users.noreply.github.com>
2026-09-07 23:53:17 +0200

    Enhance docker-installer.sh for branch and tag support
    Updated the docker installer script to handle different branches and tags for downloading docker-compose files. Improved password handling for MySQL and Redis.

--- common/docker-installer.sh
+++ common/docker-installer.sh
@@ -16,16 +16,22 @@ fi
 if [[ "$TAG" == "develop" || "$TAG" == "dev" ]]; then
     # Check if Git is installed, if not, install it
     command -v git &>/dev/null || (echo "Installing Git..."; sudo apt-get update && sudo apt-get install -y git)
-    git clone https://github.com/hiddify/Hiddify-Manager.git
+    git clone https://github.com/hiddify/Hiddify-Manager.git -b dev
     cd Hiddify-Manager
     git submodule update --init --recursive
     git submodule update --recursive --remote
     docker compose -f docker-compose.yml build
 else
-  # Create the 'hiddify-manager' directory
-  mkdir hiddify-manager
-  cd hiddify-manager
-  wget https://raw.githubusercontent.com/hiddify/Hiddify-Manager/refs/heads/main/docker-compose.yml
+    mkdir hiddify-manager
+    cd hiddify-manager
+    if [[ "$TAG" == "beta" || "$TAG" == "main" ]]; then
+        wget https://raw.githubusercontent.com/hiddify/Hiddify-Manager/refs/heads/$TAG/docker-compose.yml
+        wget https://raw.githubusercontent.com/hiddify/Hiddify-Manager/refs/heads/$TAG/docker.env
+    else
+      # Create the 'hiddify-manager' directory
+      wget https://raw.githubusercontent.com/hiddify/Hiddify-Manager/refs/tags/$TAG/docker-compose.yml
+      wget https://raw.githubusercontent.com/hiddify/Hiddify-Manager/refs/tags/$TAG/docker.env
+    fi
 fi
 
 # Generate random passwords for MySQL and Redis
@@ -34,8 +40,13 @@ redispassword=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | head -c49; echo)
 
 # Update docker-compose.yml with the specified tag and passwords
 sed -i "s/hiddify-manager:latest/hiddify-manager:$TAG/g" docker-compose.yml
-echo "REDIS_PASSWORD=$redispassword"> docker.env
-echo "MYSQL_PASSWORD=$mysqlpassword">> docker.env
+sed -i "s/redis-strong-password/$redispassword/g" docker.env
+sed -i "s/mysql-strong-password/$mysqlpassword/g" docker.env
+
+#temporary for making main version working
+sed -i "s/your-strong-password/$redispassword/g" docker.env 
+sed -i "s/another-strong-password/$mysqlpassword/g" docker.env
+
 
 # Start the containers using Docker Compose
 docker compose pull
